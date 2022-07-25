0383a5707a486bfb4dec62a8facd5673a46a948c
Cleo Gao <celeo127@outlook.com>
2022-07-25 17:09:58 +0800

    finished with axios and redux, now fetching the data in movie card

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import  MovieListing  from '../movie-listing/movie-listing.component';
+import movieBanner from '../../images/movie-banner.png';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from '../../common/apis/movieApiKey';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
+    const movieText = 'Ironman';
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchMovies = async () =>{
+            const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+            .catch((err) => {
+                console.log("Error: ", err)
+            });
+            console.log("Response from Api: ", response);
+            dispatch(addMovies(response.data));
+        }
+        fetchMovies();
+    }, []);
+
+
     return(
         <div>
-         This is home
+        <div className='banner-img'>
+            <img src={movieBanner} alt='movie-banner' />
+        </div>
+        <MovieListing />
         </div>
     )
 }
--- src/components/movie-listing/movie-listing.component.jsx
+++ src/components/movie-listing/movie-listing.component.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getAllMovies } from '../../features/movies/movieSlice';
+import MovieCard from '../../components/movie-card/movie-card.component';
+import './movie-listing.styles.scss';
 
 const MovieListing = () => {
+    const movies = useSelector(getAllMovies);
+    let renderMovies = "";
+
+    renderMovies = movies.Response === "True" ? (
+        movies.Search.map((movie, index) => (
+            <MovieCard key ={index} data={movie} />
+        ))
+    ): (
+    <div className='movies-error'>
+    <h3>{movies.Error}</h3>
+    </div>);
+
     return(
-        <div>
-         <h1>Movie Listing</h1>
+        <div className='movie-wrapper'>
+        <div className='movie-list'>
+        <h2>Movies</h2>
+        <div className='movie-container'>{renderMovies}</div>
+        </div>
+       
         </div>
     )
 }
